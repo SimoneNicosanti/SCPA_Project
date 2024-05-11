5b678211302e408ef53243592e123d5e20f154f1
EdmondDantes <snicosanti@gmail.com>
2024-05-11 19:00:13 +0200

    Begin OpenMP variant

--- project/code/OpenMp/OpenMpProduct.c
+++ project/code/OpenMp/OpenMpProduct.c
@@ -1,6 +1,22 @@
+#include "omp.h"
+#include <stdio.h>
 
-// Misto MPI/OpenMP
-// TODO > Implementa il prodotto C <- C + A * B di sequential usando OpenMP e collegalo all'implementazione MPI >> Codice meglio strutturato
 void openMpProduct(float **A, float **B, float **C, int m, int k, int n) {
+    /*
+        TODO > Potrei specificare la dimensione del chunk come la dimensione del blocco di riga??
+        In questo modo ogni thread si occupa di calcolare uno o più blocchi, però forse è meglio senza in questo modo ho
+        un carico meglio bilanciato tra i diversi thread; oppure lo faccio dynamic e per blocchi
+        TODO > Prova a cambiare ordine di ciclo
+    
+    */
+    
+    #pragma omp parallel for shared(A, B, C, m, n, k) schedule(static)
+    for (int i = 0 ; i < m ; i++) {
+        for (int t = 0 ; t < k ; t++) {
+            for (int j = 0 ; j < n ; j++) {
+                C[i][j] += A[i][t] * B[t][j] ;
+            }
+        }
+    }
     return ;
 }
