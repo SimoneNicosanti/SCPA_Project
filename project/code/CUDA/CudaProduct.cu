2a36967d53b3ee9add7e52f01cf174aa647ed91f
EdmondDantes <snicosanti@gmail.com>
2024-06-14 11:33:37 +0200

    Updated CMake to make it run on server

--- project/code/CUDA/CudaProduct.cu
+++ project/code/CUDA/CudaProduct.cu
@@ -12,22 +12,26 @@
 #define DEF_NB 50
 
 void moveMatricesFromHostToDevice(Matrix hostMatrix, Matrix *devMatrix, int rows, int cols, size_t *pitchPtr) ;
-void callKernel(Matrix A, Matrix B, Matrix C, int m, int k, int n, int pitchA, int pitchB, int pitchC) ;
+float callKernel(Matrix A, Matrix B, Matrix C, int m, int k, int n, int pitchA, int pitchB, int pitchC) ;
 
 
-void callKernel(Matrix A, Matrix B, Matrix C, int m, int k, int n, int pitchA, int pitchB, int pitchC) {
+float callKernel(Matrix A, Matrix B, Matrix C, int m, int k, int n, int pitchA, int pitchB, int pitchC) {
+
+    StopWatchInterface* timer = 0;
+    sdkCreateTimer(&timer);
 
     // Implem_4
-    const int M_BLOCK_SIZE = 64 ;
-    const int N_BLOCK_SIZE = 64 ;
+    const int M_BLOCK_SIZE = 128 ;
+    const int N_BLOCK_SIZE = 128 ;
     const int K_BLOCK_SIZE = 8 ;
 
-    const int A_TILE_SIZE = 8 ;
+    const int A_TILE_SIZE = 4 ;
     const int B_TILE_SIZE = 4 ;
 
     dim3 BLOCK_DIM((M_BLOCK_SIZE * N_BLOCK_SIZE) / (A_TILE_SIZE * B_TILE_SIZE)) ;
     dim3 GRID_DIM(((n - 1) / N_BLOCK_SIZE) + 1, ((m - 1) / M_BLOCK_SIZE) + 1) ;
 
+    timer->start();
     gpuProduct_4
     <M_BLOCK_SIZE, K_BLOCK_SIZE, N_BLOCK_SIZE, A_TILE_SIZE, B_TILE_SIZE> 
     <<<GRID_DIM, BLOCK_DIM>>>(
@@ -36,10 +40,18 @@ void callKernel(Matrix A, Matrix B, Matrix C, int m, int k, int n, int pitchA, i
         pitchA, pitchB, pitchC
     );
     checkCudaErrors(cudaDeviceSynchronize());
+    timer->stop();
+
+    return timer->getTime() ;
 
 }
 
-void CudaProduct(Matrix hostA, Matrix hostB, Matrix hostC, int m, int k, int n, int mb, int nb, Info *infoPtr) {
+void CudaProduct(
+    Matrix hostA, Matrix hostB, Matrix hostC, 
+    int m, int k, int n, 
+    int mb, int nb, 
+    Info *infoPtr
+) {
 
     if (mb <= 0) {
         mb = DEF_MB ;
@@ -54,23 +66,8 @@ void CudaProduct(Matrix hostA, Matrix hostB, Matrix hostC, int m, int k, int n, 
     moveMatricesFromHostToDevice(hostB, &devB, k, n, &pitchB) ;
     moveMatricesFromHostToDevice(hostC, &devC, m, n, &pitchC) ;
 
-    // dim3 BLOCK_DIM(BLOCK_SIZE * BLOCK_SIZE) ;
-    // dim3 GRID_DIM(((n - 1) / BLOCK_SIZE) + 1, ((m - 1) / BLOCK_SIZE) + 1) ;
-
-    // Implem_3
-    // dim3 BLOCK_DIM((BLOCK_SIZE * BLOCK_SIZE) / TILE_SIZE) ;
-    // dim3 GRID_DIM(((n - 1) / BLOCK_SIZE) + 1, ((m - 1) / BLOCK_SIZE) + 1) ;
-
-    
-
-    StopWatchInterface* timer = 0;
-    sdkCreateTimer(&timer);
-
-    timer->start();
-    callKernel(devA, devB, devC, m, k, n, pitchA, pitchB, pitchC) ;
-    timer->stop();
-    
-    infoPtr->productTime = timer->getTime() ;
+    float kernelTime = callKernel(devA, devB, devC, m, k, n, pitchA, pitchB, pitchC) ;
+    infoPtr->productTime = kernelTime ;
 
     checkCudaErrors(
         cudaMemcpy2D(hostC, sizeof(MatrixElemType) * n, devC, pitchC * sizeof(MatrixElemType), sizeof(MatrixElemType) * n, m, cudaMemcpyDeviceToHost)
